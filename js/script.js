3bd14975c671cddba8796f96f185d9e8b7c8f918
tkyssk1 <tkyssk1@gmail.com>
2022-02-20 21:18:29 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -4,40 +4,29 @@
 jQuery(function ($) {
     //変数宣言
     var win = $(window),
-        fv = $(".fv");
-    fvHeight = fv.height();
-    scr = 0;
+        // first viewの高さ取得
+        fvHeight = $(".fv").height(),
+        scr = 0;
 
     //スクロール時
     win.on("scroll load resize", function () {
         scr = win.scrollTop();
         //値のテスト用
-        console.log(fv.height());
-
+        console.log(fvHeight);
         //fv通過時
         if (scr > fvHeight) {
-            //サイドバーを開いたまま横幅を変えたときの対処
-            if(!$(".side-bar").hasClass("open")){
-                $(".header-logo").addClass("black");
-                $(".header-menu").addClass("black");    
-            }
+            $(".header").addClass("header--bg-change");
         } else {
-            $(".header-menu").removeClass("black");
-            $(".header-logo").removeClass("black");
+            $(".header").removeClass("header--bg-change");
         }
     })
 
-    //ハンバーガーボタン押下時
-    //fv通過時
+    //ハンバーガーボタン押下
     $(".header-menu").on("click", function () {
         $("body").toggleClass("noscroll");
-        $(".bg-cover").fadeToggle(300);
-        $(".burger-btn").toggleClass("close");
-        $(".side-bar").toggleClass("open");
-        //menuが黒くなっている時
-        if (scr > fvHeight) {
-            $(this).toggleClass("black");
-        }
+        $(".bg-cover").toggleClass("bg-cover--open");
+        $(".header-nav").toggleClass("header-nav--open");
+        $(".menu-icon").toggleClass("menu-icon--open");
     })
 
 });
